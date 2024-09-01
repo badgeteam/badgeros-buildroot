0a54d83391f10a804d654fe8fa14cf817fa11cd7
Hein-Pieter van Braam <hp@tmm.cx>
2024-09-01 18:34:33 +0200

    Make it easier to build newlib with our SDK
    The whole process of building newlib is a lot easier if we have a
    compiler with the same prefix as our operating system.
    
    In this case that is riscv32-badgeros or riscv64-badgeros. The SDK can
    now easily support this.

--- build-sdk.sh
+++ build-sdk.sh
@@ -17,10 +17,22 @@ function build_linux_sdk() {
 
   ${container} build -f Dockerfile.linux-builder -t badgeros-buildroot-builder-linux
   ${container} run -it --rm -v $(pwd):/tmp/buildroot:z -w /tmp/buildroot -e FORCE_UNSAFE_CONFIGURE=1 --userns=keep-id badgeros-buildroot-builder-linux bash -c "make clean; make syncconfig; make sdk"
+
+  mkdir -p badgeros-toolchains
+  tar xf output/images/${toolchain_prefix}_sdk-buildroot.tar.gz -C badgeros-toolchains
+
+  pushd badgeros-toolchains/${toolchain_prefix}_sdk-buildroot/bin
+  for f in $(ls -1 riscv32-linux-*); do ln -s ${f} $(echo $f | sed -e 's/linux/badgeros/'); done
+  popd
+
+  pushd badgeros-toolchains
+  tar -cjf ${toolchain_prefix}_sdk-buildroot.tar.bz2 ${toolchain_prefix}_sdk-buildroot
+  rm -rf ${toolchain_prefix}_sdk-buildroot
+  popd
 }
 
 build_linux_sdk
 
 echo
 echo "***************************************"
-echo "Build succesful your toolchain is in output/images/${toolchain_prefix}_sdk-buildroot.tar.gz"
+echo "Build succesful your toolchain is in badgeros-toolchains/${toolchain_prefix}_sdk-buildroot.tar.gz"
